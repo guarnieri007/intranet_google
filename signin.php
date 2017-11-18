156235b8d397374a22a2d7dae09153ba9250f377
Felipe Guarnieri <guarnieri007@gmail.com>
2017-11-18 12:12:28 -0200

    Made a new page to create new users

---
+++ signin.php
@@ -0,0 +1,60 @@
+<?php include_once("cabecalho.php");
+include_once("banco_login.php");
+include_once("banco-idiomas.php");
+include_once("banco_adm.php");
+
+$idiomas = listaIdiomas($conexao);
+$paises = buscarPaises($conexao);
+
+if (($_SESSION['usuario_logado']) == NULL) {
+?>
+
+
+  <div class="formulario">
+    <h2 class="form-cadastro-h2">Crie seu cadastro</h2>
+  <form action="criando-conta.php" method="post">
+    <label for="nome">Nome: </label>
+    <input type="text" name="nome" placeholder="digite seu nome"><br>
+
+    <label for="login">Email: </label>
+    <input type="email" name="login" placeholder="teste@teste.com"><br>
+
+    <label for="senha">Senha: </label>
+    <input type="password" name="senha"><br>
+
+    <p>Se você prefere ser chamado por outro nome ou por um apelido, digite abaixo.</p>
+    <label for="apelido">Apelido: </label>
+    <input type="text" name="apelido" placeholder="digite seu apelido" class="formulario-inputs"><br>
+  <br>
+  <label>Idioma: </label>
+  <select name="idioma" class="formulario-inputs">
+    <?php foreach ($idiomas as $idioma): ?>
+    <option value="<?= $idioma['id_idioma']; ?>"> <?= $idioma['idioma']; ?></option>
+    <?php endforeach; ?>
+  </select>
+
+
+  <label>Onde estou </label>
+  <select name="pais">
+    <?php foreach ($paises as $pais): ?>
+    <option value="<?= $pais['id_pais'] ?>"> <?= $pais['pais'] ?></option>
+    <?php endforeach; ?>
+  </select>
+
+  <br><br><br>
+  <input type="submit" value="Cadastrar" class="botao">
+
+
+  </form>
+  </div>
+  </div>
+
+  <?php
+    include_once("footer.php");
+
+
+
+} else {
+  header("Location: index.php");
+}
+ ?>
