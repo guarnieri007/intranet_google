1757492f8d5de50bd71e52017587f8cfea99242e
Felipe Guarnieri <guarnieri007@gmail.com>
2018-01-11 10:49:02 -0200

    new function to search customers by date

---
+++ pesquisarClientes.php
@@ -0,0 +1,15 @@
+<?php
+include_once("conexao.php");
+include_once("cabecalho.php");
+include_once("bClientes.php");
+include_once("class/cliente.php");
+
+$valor1 = $_GET["inicial"];
+$valor2 = $_GET["final"];
+echo $valor1;
+echo $valor2;
+?>
+
+<?php
+  include_once("footer.php");
+ ?>
